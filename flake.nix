f642c668c8065678a758876cd8ab4cad6728c660
Juan Ibiapina <juanibiapina@gmail.com>
2024-05-13 17:04:57 +0200

    chore: Update version in flake

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
         packages = rec {
           superfile = pkgs.buildGoApplication {
             pname = "superfile";
-            version = "0.1.0";
+            version = "1.1.2";
             src = ./.;
             modules = ./gomod2nix.toml;
           };
