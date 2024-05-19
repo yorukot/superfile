4d7e1c0030742a902feb6a52f5d44306856030e4
Night Cat <107802416+MHNightCat@users.noreply.github.com>
2024-05-19 18:37:00 +0800

    Update cd_on_quit

--- cd_on_quit.sh
+++ cd_on_quit.sh
@@ -1,10 +1,23 @@
 spf() {
-	export SPF_LAST_DIR="${XDG_STATE_HOME:-$HOME/.local/state}/superfile/lastdir"
-
-	command spf "$@"
-	
-	[ ! -f "$SPF_LAST_DIR" ] || {
-		. "$SPF_LAST_DIR"
-		rm -f -- "$SPF_LAST_DIR" > /dev/null
-	}
+    # Linux
+    if [ "$(uname)" == "Linux" ]; then
+        export SPF_LAST_DIR="${XDG_STATE_HOME:-$HOME/.local/state}/superfile/lastdir"
+    fi
+    
+    # macOS
+    if [ "$(uname)" == "Darwin" ]; then
+        export SPF_LAST_DIR="$HOME/Library/Application Support/superfile/lastdir"
+    fi
+    
+    # Windows
+    if [ "$(uname)" == "Windows" ]; then
+        export SPF_LAST_DIR="$LOCALAPPDATA/superfile/lastdir"
+    fi
+    
+    command spf "$@"
+    
+    [ ! -f "$SPF_LAST_DIR" ] || {
+        . "$SPF_LAST_DIR"
+        rm -f -- "$SPF_LAST_DIR" > /dev/null
+    }
 }
