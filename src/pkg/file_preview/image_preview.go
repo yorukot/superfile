bfb6d45ab6889e08b8062b387b28a3051c3d1d22
Night Cat <107802416+MHNightCat@users.noreply.github.com>
2024-05-22 19:53:38 +0800

    try to fix security problem

--- src/pkg/file_preview/image_preview.go
+++ src/pkg/file_preview/image_preview.go
@@ -20,17 +20,17 @@ func ConvertImageToANSI(img image.Image, defaultBGColor color.Color) string {
 	height := img.Bounds().Dy()
 	output := ""
 
-	r, g, b, _:= defaultBGColor.RGBA()
+	r, g, b, _ := defaultBGColor.RGBA()
 
-	BGColor := fmt.Sprintf("#%02x%02x%02x", r>>8, g>>8, b>>8)
+	BGColor := fmt.Sprintf("#%02x%02x%02x", uint8(r>>8), uint8(g>>8), uint8(b>>8))
 
 	for y := 0; y < height; y += 2 {
 		for x := 0; x < width; x++ {
 			upperColor := colorToTermenv(img.At(x, y), BGColor)
-			lowerColor := termenv.RGBColor(fmt.Sprintf("#%02x%02x%02x", r>>8, g>>8, b>>8))
+			lowerColor := termenv.RGBColor(fmt.Sprintf("#%02x%02x%02x", uint8(r>>8), uint8(g>>8), uint8(b>>8)))
 
-			if y+1 < height {
-				lowerColor = colorToTermenv(img.At(x, y+1), BGColor)
+			if y + 1 < height {
+				lowerColor = colorToTermenv(img.At(x, y + 1), BGColor)
 			}
 
 			// Using the "▄" character which fills the lower half
@@ -50,7 +50,7 @@ func colorToTermenv(c color.Color, BGColor string) termenv.RGBColor {
 		return termenv.RGBColor(BGColor)
 	}
 
-	return termenv.RGBColor(fmt.Sprintf("#%02x%02x%02x", r>>8, g>>8, b>>8))
+	return termenv.RGBColor(fmt.Sprintf("#%02x%02x%02x", uint8(r>>8), uint8(g>>8), uint8(b>>8)))
 }
 
 // Return image preview ansi string
@@ -78,11 +78,11 @@ func ImagePreview(path string, maxWidth, maxHeight int, defaultBGColor string) (
 }
 
 func hexToColor(hex string) color.RGBA {
-    hex = hex[1:]
+	hex = hex[1:]
 
-    r, _ := strconv.ParseInt(hex[0:2], 16, 0)
-    g, _ := strconv.ParseInt(hex[2:4], 16, 0)
-    b, _ := strconv.ParseInt(hex[4:6], 16, 0)
+	r, _ := strconv.ParseInt(hex[0:2], 16, 64)
+	g, _ := strconv.ParseInt(hex[2:4], 16, 64)
+	b, _ := strconv.ParseInt(hex[4:6], 16, 64)
 
-    return color.RGBA{uint8(r), uint8(g), uint8(b), 0xff} // Alpha 通道设为 0xff 表示不透明
+	return color.RGBA{uint8(r), uint8(g), uint8(b), 0xff}
 }
