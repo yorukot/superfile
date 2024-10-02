05111be5d25517be4dea5193cbf8af67bad67931
Yorukot <ben.hrc57@gmail.com>
2024-10-03 03:33:24 +0800

    update install.sh for https://github.com/yorukot/superfile/issues/410

--- website/public/install.sh
+++ website/public/install.sh
@@ -45,9 +45,9 @@ os=$(uname -s)
 
 cd "${temp_dir}"
 
-if [[ "$arch" == "x86_64" ]]; then
+if [[ "$arch" == "x86_64" || "$arch" == "amd64" ]]; then
     arch="amd64"
-elif [[ "$arch" == "arm"* ]]; then
+elif [[ "$arch" == "arm"* || "$arch" == "aarch64" || "$arch" == "arm64" ]]; then
     arch="arm64"
 else
     echo -e "${red}❌ Fail install superfile: ${yellow}Unsupported architecture${nc}"
