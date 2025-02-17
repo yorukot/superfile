1fddff2d59a95c0b5a0429b7468d3ba111744190
Nitin Kumar <nknitinkumar12384@gmail.com>
2025-02-17 21:37:01 +0530

    cicd: Remove unnecessary debug steps

--- testsuite/core/test_constants.py
+++ testsuite/core/test_constants.py
@@ -4,4 +4,5 @@ KEY_DELAY : float       = 0.05 # seconds
 OPERATION_DELAY : float = 0.3 # seconds
 
 # 0.3 second was too less for windows
-CLOSE_WAIT_TIME : float     = 0.5 # seconds
+# 0.5 second Github workflow failed for with superfile is still running errors
+CLOSE_WAIT_TIME : float     = 1 # seconds
