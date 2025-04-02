f3e38ee54e2f98deb0be0af7227803221b6eab74
Nitin Kumar <nknitinkumar12384@gmail.com>
2025-04-02 08:46:49 +0530

    feat: Add test for promptModal for basic getPromptAction validation

--- src/internal/ui/prompt/core_test.go
+++ src/internal/ui/prompt/core_test.go
@@ -5,39 +5,36 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/yorukot/superfile/src/internal/common"
 	"github.com/yorukot/superfile/src/internal/common/utils"
+	"log/slog"
+	"os"
 	"testing"
 )
 
 var setupDone = false
 
 // Initialize the globals we need for testing
-func initGlobals() {
+func initGlobals(initLogging bool) {
 	// Maybe we could use doOnce go constructs
 	if setupDone {
 		return
 	}
+	if initLogging {
+		slog.SetDefault(slog.New(slog.NewTextHandler(
+			os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})))
+	}
 	common.Hotkeys.ConfirmTyping = []string{"enter"}
-	common.Hotkeys.ConfirmTyping = []string{"ctrl+c", "esc"}
+	common.Hotkeys.CancelTyping = []string{"ctrl+c", "esc"}
 	setupDone = true
 }
 
-type modelValidator func(*testing.T, Model)
-
-type update struct {
-	input          tea.Msg
-	validator      modelValidator
-	expectedAction common.ModelAction
-}
-
-// Validator
-func alwaysOk(_ *testing.T, m Model) {
-}
-
 func TestModel_HandleUpdate(t *testing.T) {
+	const defaultCwd = "/"
+	// Could take this as an test arguement
+	initGlobals(true)
 	// We want to test
 	// 1. Handle update called on closed Model
 	// 2. Pressing confirm on empty input
-	// 3. Three conditions of getPromptAction -
+	// 3. Three conditions of getPromptAction
 	// 4. Cancel typing
 	// 5. Switching between shell and SPF mode
 	// 6. Updating text input with text - single and multi char string
@@ -45,59 +42,55 @@ func TestModel_HandleUpdate(t *testing.T) {
 	// Validate blink m.textInput.Cursor.Blink
 	// Dont test getPromptAction here
 
-	testdata := []struct {
-		name           string
-		m              Model
-		initialization func(*Model)
+	t.Run("Handle update called on closed Model", func(t *testing.T) {
+		m := GenerateModel(spfPromptChar, shellPromptChar, true)
+		action, _ := m.HandleUpdate(utils.TeaRuneKeyMsg("x"), "/")
+		assert.False(t, m.IsOpen())
+		assert.Equal(t, common.NoAction{}, action)
+	})
 
-		updates []update
-	}{
-		{
-			// Default Model is closed
-			name: "Handle update called on closed Model",
-			m:    GenerateModel(spfPromptChar, shellPromptChar, true),
+	t.Run("Pressing confirm on empty input", func(t *testing.T) {
 
-			initialization: func(m *Model) {},
-			updates: []update{
-				{
-					input: utils.TeaRuneKeyMsg("x"),
-					validator: func(t *testing.T, m Model) {
-						assert.False(t, m.IsOpen())
-					},
-					expectedAction: common.NoAction{},
-				},
-			},
-		},
-		{
-			// Default Model is closed
-			name: "Pressing confirm on empty input",
-			m:    GenerateModel(spfPromptChar, shellPromptChar, true),
+		actualTest := func(closeOnSuccess bool, openAfterEnter bool) {
+			m := GenerateModel(spfPromptChar, shellPromptChar, closeOnSuccess)
+			m.Open(true)
+			assert.True(t, m.IsOpen())
 
-			initialization: func(m *Model) {
-				m.Open(true)
-			},
-			updates: []update{
-				{
-					input: tea.KeyMsg{Type: tea.KeyEnter},
-					validator: func(t *testing.T, m Model) {
-						assert.False(t, m.IsOpen())
-					},
-					expectedAction: common.NoAction{},
-				},
-			},
-		},
-	}
+			action, _ := m.HandleUpdate(tea.KeyMsg{Type: tea.KeyEnter}, defaultCwd)
+			assert.Equal(t, openAfterEnter, m.IsOpen())
+			assert.Equal(t, common.NoAction{}, action)
+			assert.Equal(t, "", m.resultMsg)
+			assert.Equal(t, true, m.actionSuccess)
+		}
 
-	for _, tt := range testdata {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.initialization(&tt.m)
-			for _, u := range tt.updates {
-				// Todo : Replace cwd with this file's directory
-				action, _ := tt.m.HandleUpdate(u.input, "/")
-				assert.Equal(t, u.expectedAction, action)
-				u.validator(t, tt.m)
-			}
+		actualTest(true, false)
+		actualTest(false, true)
 
-		})
-	}
+	})
+
+	t.Run("Validate Prompt Actions", func(t *testing.T) {
+		m := GenerateModel(spfPromptChar, shellPromptChar, true)
+		m.Open(false)
+
+		action, _ := m.HandleUpdate(utils.TeaRuneKeyMsg(splitCommand), defaultCwd)
+		assert.Equal(t, common.NoAction{}, action)
+
+		action, _ = m.HandleUpdate(tea.KeyMsg{Type: tea.KeyEnter}, defaultCwd)
+		assert.Equal(t, common.SplitPanelAction{}, action)
+
+		action, _ = m.HandleUpdate(utils.TeaRuneKeyMsg("bad_command"), defaultCwd)
+		action, _ = m.HandleUpdate(tea.KeyMsg{Type: tea.KeyEnter}, defaultCwd)
+		assert.Equal(t, common.NoAction{}, action)
+		assert.False(t, m.actionSuccess)
+		assert.NotEmpty(t, m.resultMsg)
+
+		m.shellMode = true
+		command := "abc def /xyz"
+		action, _ = m.HandleUpdate(utils.TeaRuneKeyMsg(command), defaultCwd)
+		action, _ = m.HandleUpdate(tea.KeyMsg{Type: tea.KeyEnter}, defaultCwd)
+		assert.Equal(t, common.ShellCommandAction{Command: command}, action)
+
+		// Todo : test third error condition .
+
+	})
 }
