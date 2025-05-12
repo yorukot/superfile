29c2b3ecc5e345bfbc8970d229110e37103ffc26
Nitin Kumar <nknitinkumar12384@gmail.com>
2025-05-12 06:51:35 +0530

    fix: Unit test fix for windows
    fix: Unit test fix for windows 2

--- src/internal/utils/file_utils_test.go
+++ src/internal/utils/file_utils_test.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"path/filepath"
+	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/adrg/xdg"
@@ -8,6 +11,16 @@ import (
 )
 
 func TestResolveAbsPath(t *testing.T) {
+	sep := string(filepath.Separator)
+	dir1 := "abc"
+	dir2 := "def"
+
+	absPrefix := ""
+	if runtime.GOOS == "windows" {
+		absPrefix = "C:" // Windows absolute path prefix
+	}
+	root := absPrefix + sep
+
 	testdata := []struct {
 		name        string
 		cwd         string
@@ -16,39 +29,39 @@ func TestResolveAbsPath(t *testing.T) {
 	}{
 		{
 			name:        "Path cleaup Test 1",
-			cwd:         "/",
-			path:        "////",
-			expectedRes: "/",
+			cwd:         absPrefix + sep,
+			path:        absPrefix + strings.Repeat(sep, 10),
+			expectedRes: absPrefix + sep,
 		},
 		{
 			name:        "Basic test",
-			cwd:         "/abc",
-			path:        "def",
-			expectedRes: "/abc/def",
+			cwd:         filepath.Join(root, dir1),
+			path:        dir2,
+			expectedRes: filepath.Join(root, dir1, dir2),
 		},
 		{
 			name:        "Ignore cwd for abs path",
-			cwd:         "/abc",
-			path:        "/def",
-			expectedRes: "/def",
+			cwd:         filepath.Join(root, dir1),
+			path:        filepath.Join(root, dir2),
+			expectedRes: filepath.Join(root, dir2),
 		},
 		{
 			name:        "Path cleanup Test 2",
-			cwd:         "///abc",
-			path:        "./././def",
-			expectedRes: "/abc/def",
+			cwd:         absPrefix + strings.Repeat(sep, 4) + dir1,
+			path:        "." + sep + "." + sep + dir2,
+			expectedRes: filepath.Join(root, dir1, dir2),
 		},
 		{
 			name:        "Basic test with ~",
-			cwd:         "/",
+			cwd:         root,
 			path:        "~",
 			expectedRes: xdg.Home,
 		},
 		{
 			name:        "~ should not be resolved if not first",
-			cwd:         "abc",
-			path:        "x/~",
-			expectedRes: "abc/x/~",
+			cwd:         dir1,
+			path:        filepath.Join(dir2, "~"),
+			expectedRes: filepath.Join(dir1, dir2, "~"),
 		},
 	}
 
