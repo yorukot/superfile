fd42dcf00ace66c8f19bd34877525d7157d70c56
Matt <mattjared8@gmail.com>
2025-05-22 12:22:03 +0200

    Deleted reduntant error , edit function getZipArchiveName, comment out old zipSource function

--- src/internal/file_operations_compress.go
+++ src/internal/file_operations_compress.go
@@ -2,186 +2,340 @@ package internal
 
 import (
 	"archive/zip"
+
 	"io"
-	"log/slog"
+
 	"os"
+
 	"path/filepath"
 
-	"github.com/charmbracelet/bubbles/progress"
-	"github.com/lithammer/shortuuid"
-	"github.com/yorukot/superfile/src/config/icon"
-	"github.com/yorukot/superfile/src/internal/common"
+	"strings"
 )
 
-func zipSource(source, target string) error {
-	id := shortuuid.New()
-	prog := progress.New()
-	prog.PercentageStyle = common.FooterStyle
+/*
 
-	totalFiles, err := countFiles(source)
+ func zipSource(source, target string) error {
 
-	if err != nil {
-		slog.Error("Error while zip file count files ", "error", err)
-	}
+ 	 id := shortuuid.New()
 
-	p := process{
-		name:     "zip files",
-		progress: prog,
-		state:    inOperation,
-		total:    totalFiles,
-		done:     0,
-	}
+ 	 prog := progress.New()
 
-	message := channelMessage{
-		messageID:       id,
-		messageType:     sendProcess,
-		processNewState: p,
-	}
+ 	 prog.PercentageStyle = common.FooterStyle
 
-	_, err = os.Stat(target)
-	if os.IsExist(err) {
-		p.name = icon.CompressFile + icon.Space + "File already exist"
-		message.processNewState = p
-		channel <- message
-		return nil
-	}
 
-	f, err := os.Create(target)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
 
-	writer := zip.NewWriter(f)
-	defer writer.Close()
+ 	 totalFiles, err := countFiles(source)
 
-	err = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
-		p.name = icon.CompressFile + icon.Space + filepath.Base(path)
-		if len(channel) < 5 {
-			message.processNewState = p
-			channel <- message
-		}
 
-		if err != nil {
-			return err
-		}
 
-		header, err := zip.FileInfoHeader(info)
-		if err != nil {
-			return err
-		}
+ 	 if err != nil {
 
-		header.Method = zip.Deflate
+ 	 	 slog.Error("Error while zip file count files ", "error", err)
 
-		header.Name, err = filepath.Rel(filepath.Dir(source), path)
-		if err != nil {
-			return err
-		}
-		if info.IsDir() {
-			header.Name += "/"
-		}
+ 	 }
 
-		headerWriter, err := writer.CreateHeader(header)
-		if err != nil {
-			return err
-		}
 
-		if info.IsDir() {
-			return nil
-		}
 
-		f, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
+ 	 p := process{
 
-		_, err = io.Copy(headerWriter, f)
-		if err != nil {
-			return err
-		}
-		p.done++
-		if len(channel) < 5 {
-			message.processNewState = p
-			channel <- message
-		}
-		return nil
-	})
+ 	 	 name:     "zip files",
 
-	if err != nil {
-		slog.Error("Error while zip file", "error", err)
-		p.state = failure
-		message.processNewState = p
-		channel <- message
-	}
-	p.state = successful
-	p.done = totalFiles
+ 	 	 progress: prog,
 
-	message.processNewState = p
-	channel <- message
+ 	 	 state:    inOperation,
 
-	return nil
-}
+ 	 	 total:    totalFiles,
+
+ 	 	 done:     0,
+
+ 	 }
+
+
+
+ 	 message := channelMessage{
+
+ 	 	 messageID:       id,
+
+ 	 	 messageType:     sendProcess,
+
+ 	 	 processNewState: p,
+
+ 	 }
+
+
+
+ 	 _, err = os.Stat(target)
+
+ 	 if os.IsExist(err) {
+
+ 	 	 p.name = icon.CompressFile + icon.Space + "File already exist"
+
+ 	 	 message.processNewState = p
+
+ 	 	 channel <- message
+
+ 	 	 return nil
+
+ 	 }
+
+
+
+ 	 f, err := os.Create(target)
+
+ 	 if err != nil {
+
+ 	 	 return err
+
+ 	 }
+
+ 	 defer f.Close()
+
+
+
+ 	 writer := zip.NewWriter(f)
+
+ 	 defer writer.Close()
+
+
+
+ 	 err = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+
+ 	 	 p.name = icon.CompressFile + icon.Space + filepath.Base(path)
+
+ 	 	 if len(channel) < 5 {
+
+ 	 	 	 message.processNewState = p
+
+ 	 	 	 channel <- message
+
+ 	 	 }
+
+
+
+ 	 	 if err != nil {
+
+ 	 	 	 return err
+
+ 	 	 }
+
+
+
+ 	 	 header, err := zip.FileInfoHeader(info)
+
+ 	 	 if err != nil {
+
+ 	 	 	 return err
+
+ 	 	 }
+
+
+
+ 	 	 header.Method = zip.Deflate
+
+
+
+ 	 	 header.Name, err = filepath.Rel(filepath.Dir(source), path)
+
+ 	 	 if err != nil {
+
+ 	 	 	 return err
+
+ 	 	 }
+
+ 	 	 if info.IsDir() {
+
+ 	 	 	 header.Name += "/"
+
+ 	 	 }
+
+
+
+ 	 	 headerWriter, err := writer.CreateHeader(header)
+
+ 	 	 if err != nil {
+
+ 	 	 	 return err
+
+ 	 	 }
+
+
+
+ 	 	 if info.IsDir() {
+
+ 	 	 	 return nil
+
+ 	 	 }
+
+
+
+ 	 	 f, err := os.Open(path)
+
+ 	 	 if err != nil {
+
+ 	 	 	 return err
+
+ 	 	 }
+
+ 	 	 defer f.Close()
+
+
+
+ 	 	 _, err = io.Copy(headerWriter, f)
+
+ 	 	 if err != nil {
+
+ 	 	 	 return err
+
+ 	 	 }
+
+ 	 	 p.done++
+
+ 	 	 if len(channel) < 5 {
+
+ 	 	 	 message.processNewState = p
+
+ 	 	 	 channel <- message
+
+ 	 	 }
+
+ 	 	 return nil
+
+ 	 })
+
+
+
+ 	 if err != nil {
+
+ 	 	 slog.Error("Error while zip file", "error", err)
+
+ 	 	 p.state = failure
+
+ 	 	 message.processNewState = p
+
+ 	 	 channel <- message
+
+ 	 }
+
+ 	 p.state = successful
+
+ 	 p.done = totalFiles
+
+
+
+ 	 message.processNewState = p
+
+ 	 channel <- message
+
+
+
+ 	 return nil
+
+ }
+
+*/
 
 func zipSources(sources []string, target string) error {
+
 	f, err := os.Create(target)
+
 	if err != nil {
+
 		return err
+
 	}
+
 	defer f.Close()
+
 	writer := zip.NewWriter(f)
+
 	defer writer.Close()
 
 	for _, src := range sources {
-		if err != nil {
-			return err
-		}
 
 		srcParentDir := filepath.Dir(src)
 
 		err = filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+
 			if err != nil {
+
 				return err
+
 			}
 
 			relPath, err := filepath.Rel(srcParentDir, path)
+
 			if err != nil {
+
 				return err
+
 			}
 
 			header, err := zip.FileInfoHeader(info)
+
 			if err != nil {
+
 				return err
+
 			}
+
 			header.Method = zip.Deflate
 
 			header.Name = relPath
 
 			if info.IsDir() {
+
 				header.Name += "/"
+
 			}
 
 			hw, err := writer.CreateHeader(header)
+
 			if err != nil {
+
 				return err
+
 			}
 
 			if info.IsDir() {
+
 				return nil
+
 			}
 
 			file, err := os.Open(path)
+
 			if err != nil {
+
 				return err
+
 			}
+
 			defer file.Close()
+
 			_, err = io.Copy(hw, file)
+
 			return err
+
 		})
 
 		if err != nil {
+
 			return err
+
 		}
+
 	}
+
 	return nil
+
+}
+
+func getZipArchiveName(base string) (string, error) {
+
+	zipName := strings.TrimSuffix(base, filepath.Ext(base)) + ".zip"
+
+	zipName, err := renameIfDuplicate(zipName)
+
+	return zipName, err
+
 }
--- src/internal/handle_file_operations.go
+++ src/internal/handle_file_operations.go
@@ -527,6 +527,89 @@ func (m *model) extractFile() {
 	}
 }
 
+/*
+// Compress file or directory
+func (m *model) compressFile() {
+	 panel := &m.fileModel.filePanels[m.filePanelFocusIndex]
+
+	 if len(panel.element) == 0 {
+	 	 return
+	 }
+	 //var fileName string
+
+	 if len(panel.selected) == 0 {
+	 	 fileName := filepath.Base(panel.element[panel.cursor].location)
+	 	 zipName := strings.TrimSuffix(fileName, filepath.Ext(fileName)) + ".zip"
+	 	 zipName, err := renameIfDuplicate(zipName)
+
+	 	 if err != nil {
+	 	 	 slog.Error("Error in compressing files during rename duplicate", "error", err)
+	 	 	 return
+	 	 }
+
+	 	 err = zipSource(panel.element[panel.cursor].location, filepath.Join(filepath.Dir(panel.element[panel.cursor].location), zipName))
+	 	 if err != nil {
+	 	 	 slog.Error("Error in zipping files", "error", err)
+	 	 	 // Although return is not needed here at the moment. This clarifies the intent of
+	 	 	 // not continuing after the error even if any further code is added in this function later
+	 	 	 return
+	 	 }
+	 } else {
+	 	 tempDir, err := os.MkdirTemp("", "temp-dir")
+	 	 if err != nil {
+	 	 	 slog.Error("Error in creating temp dir", "error", err)
+	 	 	 return
+	 	 }
+	 	 defer os.RemoveAll(tempDir)
+
+	 	 if len(panel.selected) == 0 {
+	 	 	 slog.Error("No elements selected")
+	 	 	 return
+	 	 }
+
+	 	 for _, elem := range panel.selected {
+	 	 	 baseName := filepath.Base(elem)
+	 	 	 destPath := filepath.Join(tempDir, baseName)
+
+	 	 	 fileInfo, err := os.Stat(elem)
+	 	 	 if err != nil {
+	 	 	 	 slog.Error("Error getting file info", "element", elem, "error", err)
+	 	 	 	 continue
+	 	 	 }
+
+	 	 	 if fileInfo.IsDir() {
+	 	 	 	 err = copyDir(elem, destPath, fileInfo)
+	 	 	 } else {
+	 	 	 	 err = copyFile(elem, destPath, fileInfo)
+	 	 	 }
+
+	 	 	 if err != nil {
+	 	 	 	 slog.Error("Error copying element", "element", elem, "error", err)
+	 	 	 	 continue
+	 	 	 }
+	 	 }
+
+	 	 firstSelectedName := filepath.Base(panel.selected[0])
+	 	 zipName := strings.TrimSuffix(firstSelectedName, filepath.Ext(firstSelectedName)) + ".zip"
+
+	 	 zipName, err = renameIfDuplicate(zipName)
+	 	 if err != nil {
+	 	 	 slog.Error("Error in compressing files during rename duplicate", "error", err)
+	 	 	 return
+	 	 }
+
+	 	 currentDir := filepath.Dir(panel.element[panel.cursor].location)
+	 	 zipPath := filepath.Join(currentDir, zipName)
+
+	 	 err = zipSource(tempDir, zipPath)
+	 	 if err != nil {
+	 	 	 slog.Error("Error in zipping files", "error", err)
+	 	 	 return
+	 	 }
+	 }
+}
+*/
+
 func (m *model) compressFile() {
 	panel := &m.fileModel.filePanels[m.filePanelFocusIndex]
 
@@ -536,8 +619,7 @@ func (m *model) compressFile() {
 
 	if len(panel.selected) == 0 {
 		fileName := filepath.Base(panel.element[panel.cursor].location)
-		zipName := strings.TrimSuffix(fileName, filepath.Ext(fileName)) + ".zip"
-		zipName, err := renameIfDuplicate(zipName)
+		zipName, err := getZipArchiveName(fileName)
 		if err != nil {
 			slog.Error("Error in compressing files during rename duplicate", "error", err)
 			return
@@ -553,8 +635,7 @@ func (m *model) compressFile() {
 	} else {
 		first := panel.selected[0]
 		base := filepath.Base(first)
-		zipName := strings.TrimSuffix(base, filepath.Ext(base)) + ".zip"
-		zipName, err := renameIfDuplicate(zipName)
+		zipName, err := getZipArchiveName(base)
 		if err != nil {
 			slog.Error("Error in compressing files during rename duplicate", "error", err)
 			return
