66e358e4388bbc9b8756f97d8b7235d975ad554e
Ahmed HABBACHI <a.habbachi@case-tunisia.com>
2025-07-02 14:59:45 +0100

    feat: Add a new hotkey to handle cd-on-quit whenever needed

--- src/internal/model.go
+++ src/internal/model.go
@@ -233,6 +233,7 @@ func (m *model) handleKeyInput(msg tea.KeyMsg) tea.Cmd {
 	}
 	var cmd tea.Cmd
 	quitSuperfile := false
+	cdOnQuit := common.Config.CdOnQuit
 	switch {
 	case m.typingModal.open:
 		m.typingModalOpenKey(msg.String())
@@ -268,6 +269,10 @@ func (m *model) handleKeyInput(msg tea.KeyMsg) tea.Cmd {
 	case slices.Contains(common.Hotkeys.Quit, msg.String()):
 		m.modelQuitState = quitInitiated
 
+	case slices.Contains(common.Hotkeys.CdQuit, msg.String()):
+		m.modelQuitState = quitInitiated
+		cdOnQuit = true
+
 	default:
 		// Handles general kinds of inputs in the regular state of the application
 		cmd = m.mainKey(msg.String())
@@ -283,7 +288,7 @@ func (m *model) handleKeyInput(msg tea.KeyMsg) tea.Cmd {
 		quitSuperfile = true
 	}
 	if quitSuperfile {
-		m.quitSuperfile()
+		m.quitSuperfile(cdOnQuit)
 		return tea.Quit
 	}
 	return cmd
@@ -587,7 +592,7 @@ func (m *model) getFilePanelItems() {
 
 // Close superfile application. Cd into the current dir if CdOnQuit on and save
 // the path in state direcotory
-func (m *model) quitSuperfile() {
+func (m *model) quitSuperfile(cdOnQuit bool) {
 	// close exiftool session
 	if common.Config.Metadata && et != nil {
 		et.Close()
@@ -596,7 +601,7 @@ func (m *model) quitSuperfile() {
 	currentDir := m.fileModel.filePanels[m.filePanelFocusIndex].location
 	variable.SetLastDir(currentDir)
 
-	if common.Config.CdOnQuit {
+	if cdOnQuit {
 		// escape single quote
 		currentDir = strings.ReplaceAll(currentDir, "'", "'\\''")
 		err := os.WriteFile(variable.LastDirFile, []byte("cd '"+currentDir+"'"), 0755)
