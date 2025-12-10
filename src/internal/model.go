b3231234003658242b2799bb54fe7ddf9f105ffe
lazysegtree <59679977+lazysegtree@users.noreply.github.com>
2025-12-10 13:17:36 +0530

    fix: group resource cleanup together

--- src/internal/model.go
+++ src/internal/model.go
@@ -745,10 +745,12 @@ func (m *model) getFilePanelItems() {
 // Close superfile application. Cd into the current dir if CdOnQuit on and save
 // the path in state direcotory
 func (m *model) quitSuperfile(cdOnQuit bool) {
-	// close exiftool session
+	// Resource cleanup
 	if common.Config.Metadata && et != nil {
 		et.Close()
 	}
+	m.fileModel.filePreview.CleanUp()
+
 	// cd on quit
 	currentDir := m.fileModel.filePanels[m.filePanelFocusIndex].location
 	variable.SetLastDir(currentDir)
@@ -761,7 +763,6 @@ func (m *model) quitSuperfile(cdOnQuit bool) {
 			slog.Error("Error during writing lastdir file", "error", err)
 		}
 	}
-	m.fileModel.filePreview.CleanUp()
 	m.modelQuitState = quitDone
 	slog.Debug("Quitting superfile", "current dir", currentDir)
 }
