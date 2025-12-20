3a939e73e092e1fc828ce99d98a011211f1f2811
lazysegtree <59679977+lazysegtree@users.noreply.github.com>
2025-12-20 19:19:23 +0530

    fix: Separate types

--- src/internal/type.go
+++ src/internal/type.go
@@ -1,9 +1,6 @@
 package internal
 
 import (
-	"os"
-	"time"
-
 	zoxidelib "github.com/lazysegtree/go-zoxide"
 
 	"github.com/yorukot/superfile/src/internal/ui/metadata"
@@ -150,56 +147,6 @@ type fileModel struct {
 	renaming     bool
 	maxFilePanel int
 	filePreview  preview.Model
-}
-
-// Panel representing a file
-type FilePanel struct {
-	Cursor      int
-	RenderIndex int
-	IsFocused   bool
-	Location    string
-	// TODO: Every file panel doesn't needs sort options model
-	// They just need to store their current sort config.
-	SortOptions        SortOptionsModel
-	PanelMode          PanelMode
-	Selected           []string
-	Element            []Element
-	DirectoryRecords   map[string]DirectoryRecord
-	Rename             textinput.Model
-	Renaming           bool
-	SearchBar          textinput.Model
-	LastTimeGetElement time.Time
-	TargetFile         string // filename to position cursor on after load
-}
-
-// Sort options
-type SortOptionsModel struct {
-	Width  int
-	Height int
-	Open   bool
-	Cursor int
-	Data   SortOptionsModelData
-}
-
-type SortOptionsModelData struct {
-	Options  []string
-	Selected int
-	Reversed bool
-}
-
-// Record for directory navigation
-type DirectoryRecord struct {
-	DirectoryCursor int
-	DirectoryRender int
-}
-
-// Element within a file panel
-type Element struct {
-	Name      string
-	Location  string
-	Directory bool
-	MetaData  [][2]string
-	Info      os.FileInfo
 }
 
 /* FILE WINDOWS TYPE END*/
