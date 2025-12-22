bec65c080c4576ba998677f9d01f64b20d45a2b8
Nitin Kumar <nknitinkumar12384@gmail.com>
2025-12-22 15:04:10 +0530

    fix: Remove unnecessary comment

--- src/internal/ui/filepanel/dimension.go
+++ src/internal/ui/filepanel/dimension.go
@@ -2,13 +2,11 @@ package filepanel
 
 import "github.com/yorukot/superfile/src/internal/common"
 
-// UpdateDimensions sets the panel dimensions with validation
 func (m *Model) UpdateDimensions(width, height int) {
 	m.SetWidth(width)
 	m.SetHeight(height)
 }
 
-// GetWidth returns the total panel width
 func (m *Model) SetWidth(width int) {
 	if width < FilePanelMinWidth {
 		width = FilePanelMinWidth
@@ -26,22 +24,18 @@ func (m *Model) SetHeight(height int) {
 	m.scrollToCursor(m.Cursor)
 }
 
-// GetWidth returns the total panel width
 func (m *Model) GetWidth() int {
 	return m.width
 }
 
-// GetHeight returns the total panel height
 func (m *Model) GetHeight() int {
 	return m.height
 }
 
-// GetMainPanelHeight returns content height (total height minus borders)
 func (m *Model) GetMainPanelHeight() int {
 	return m.height - common.BorderPadding
 }
 
-// GetContentWidth returns content width (total width minus borders)
 func (m *Model) GetContentWidth() int {
 	return m.width - common.BorderPadding
 }
