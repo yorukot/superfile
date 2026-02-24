532f27cf9b5ea10f1bee4dd468df932e7447d70f
Nitin Kumar <nknitinkumar12384@gmail.com>
2026-02-25 02:34:20 +0530

    fix: Remove unnecessary setting of cmd.Stdin

--- src/internal/utils/detach_unix.go
+++ src/internal/utils/detach_unix.go
@@ -7,15 +7,13 @@ import (
 	"syscall"
 )
 
-func DetachFromTerminal(cmd *exec.Cmd, keepStdoutAndStderr bool) {
+func DetachFromTerminal(cmd *exec.Cmd, keepStdio bool) {
 	// Start new session so child isn't tied to the TTY (prevents SIGHUP on terminal close).
 	// This also prevents programs like sudo to read/write to tty
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
-
 	// Optionally, redirect stdio to avoid terminal hangups
-	// Stdin set to nil is also needed to prevent interactive commands in shell mode
-	cmd.Stdin = nil
-	if !keepStdoutAndStderr {
+	if !keepStdio {
+		cmd.Stdin = nil
 		cmd.Stdout = nil
 		cmd.Stderr = nil
 	}
