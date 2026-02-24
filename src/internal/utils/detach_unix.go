f258ba3dc4f3a52f9ed465eeda05955a82ed73db
Nitin Kumar <nknitinkumar12384@gmail.com>
2026-02-25 02:22:44 +0530

    fix: Fix comments

--- src/internal/utils/detach_unix.go
+++ src/internal/utils/detach_unix.go
@@ -9,9 +9,11 @@ import (
 
 func DetachFromTerminal(cmd *exec.Cmd, keepStdoutAndStderr bool) {
 	// Start new session so child isn't tied to the TTY (prevents SIGHUP on terminal close).
-	// This also prevents programs like sudo cannot open and read/write to tty
+	// This also prevents programs like sudo to read/write to tty
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
+
 	// Optionally, redirect stdio to avoid terminal hangups
+	// Stdin set to nil is also needed to prevent interactive commands in shell mode
 	cmd.Stdin = nil
 	if !keepStdoutAndStderr {
 		cmd.Stdout = nil
