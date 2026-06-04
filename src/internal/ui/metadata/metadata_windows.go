2263d89d01b6e39cda1fb07a12f43cf03f326b63
Alex Klepov <alexezm@narod.ru>
2026-06-04 18:39:35 +0300

    win attrs

--- src/internal/ui/metadata/metadata_windows.go
+++ src/internal/ui/metadata/metadata_windows.go
@@ -4,6 +4,9 @@ package metadata
 
 import (
 	"os"
+	"syscall"
+
+	"golang.org/x/sys/windows"
 )
 
 func getOwnerAndGroup(_ os.FileInfo) (string, string) {
@@ -11,7 +14,30 @@ func getOwnerAndGroup(_ os.FileInfo) (string, string) {
 }
 
 // returns file attributes
-// TODO: need realisation
-func getFileAttributes(_ string) (string, bool) {
-	return "", false
+func getFileAttributes(path string) (string, bool) {
+	ptr, err := syscall.UTF16PtrFromString(path)
+	if err != nil {
+		return "", false
+	}
+	attrs, err := windows.GetFileAttributes(ptr)
+	if err != nil {
+		return "", false
+	}
+	result := ""
+	if attrs&syscall.FILE_ATTRIBUTE_READONLY != 0 {
+		result += "R"
+	}
+	if attrs&syscall.FILE_ATTRIBUTE_HIDDEN != 0 {
+		result += "H"
+	}
+	if attrs&syscall.FILE_ATTRIBUTE_SYSTEM != 0 {
+		result += "S"
+	}
+	if attrs&syscall.FILE_ATTRIBUTE_ARCHIVE != 0 {
+		result += "A"
+	}
+	if attrs&syscall.FILE_ATTRIBUTE_DIRECTORY != 0 {
+		result += "D"
+	}
+	return result, true
 }
