44b2d20a380995b360b657fe0770fe71fa2f2f67
Alex Klepov <alexezm@yandex.ru>
2026-06-17 20:28:54 +0300

    test for different os

---
+++ src/internal/common/string_function_windows_test.go
@@ -0,0 +1,28 @@
+package common
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFilenameWithouTextWindows(t *testing.T) {
+	var inputs = []struct {
+		input    string
+		expected string
+	}{
+		{"c:\\home\\user\\temp\\.tmp\\.dockerignore.zip", "c:\\home\\user\\temp\\.tmp\\.dockerignore"},
+		{"c:\\home\\user\\temp\\.tmp\\.dockerignore", "c:\\home\\user\\temp\\.tmp\\.dockerignore"},
+		{"c:\\tmp\\aaa.bbb\\file", "c:\\tmp\\aaa.bbb\\file"},
+		{"c:\\tmp\\aaa.bbb\\.file", "c:\\tmp\\aaa.bbb\\.file"},
+		{"c:\\tmp\\aaa.bbb\\.file.txt", "c:\\tmp\\aaa.bbb\\.file"},
+	}
+
+	for _, tt := range inputs {
+		t.Run(fmt.Sprintf("Remove extension from %s", tt.input), func(t *testing.T) {
+			result := FileNameWithoutExtension(tt.input)
+			if result != tt.expected {
+				t.Errorf("Expected %s, got %s", tt.expected, result)
+			}
+		})
+	}
+}
